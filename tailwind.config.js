512619037ecf3012bfd9f5251ad48e17aaa14158
ethanhelwig <ethanahelwig@gmail.com>
2022-09-23 14:27:52 -0400

    Created a landing page with sign-in and login pages.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        t1: {
+          bluegray: '#203C48',
+          seagreen: '#22958B',
+          lightgreen: '#AAD5AC',
+          sand: '#EDEAC7',
+          cream: '#EDF0DB',
+        },
+      },
+    },
   },
   plugins: [],
 };
