58df2da7cfb63655f9dcc24b7fc3a117b624ea65
jackharrison300 <78267252+jackharrison300@users.noreply.github.com>
2022-09-26 19:35:19 -0400

    landing page cognito integration

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,27 @@
-placeholder for deployment shell script
+# make layer for dependencies
+cp {package.json,package-lock.json} layer/nodejs
+npm install --omit=dev --prefix layer/nodejs
+cd layer
+rm layer.zip
+zip -r layer.zip nodejs
+cd ..
+
+# build app
+rm -r server/*
+npm run build
+
+# deploy
+sam deploy
+
+# set cognito callback/logout url to api url
+# note that if we try to do this in the sam template, we get a circular dependency
+# (unless we reverse the dependency and store the cognito client id in parameter store,
+# which would also be a reasonable approach, though would have a slight performance cost)
+cognito_client_id=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='t25prod'][].Outputs[?OutputKey=='UserPoolClientId'].OutputValue" --output text)
+cognito_userpool_id=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='t25prod'][].Outputs[?OutputKey=='UserPoolId'].OutputValue" --output text)
+api_url=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='t25prod'][].Outputs[?OutputKey=='API'].OutputValue" --output text)
+aws cognito-idp update-user-pool-client --user-pool-id $cognito_userpool_id --client-id $cognito_client_id --callback-urls $api_url --logout-urls $api_url
+
+# s3 upload
+sbucket=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='t25prod'][].Outputs[?OutputKey=='StaticBucket'].OutputValue" --output text)
+aws s3 cp public/. $sbucket --recursive
