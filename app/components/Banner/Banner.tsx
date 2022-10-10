661884642b749790322268d21c25d668581ad065
jackharrison300 <78267252+jackharrison300@users.noreply.github.com>
2022-10-10 19:22:32 -0400

    form fix

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -5,18 +5,6 @@ import { PersonalInfo, DriverInfo } from '../../routes/app'
 {/* Default pfp-less image */ }
 let defaultimg = "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
 
-{/* the text within the button for the dropdown menu */ }
-const dropDownButton = [
-  { name: '≡', current: false }
-]
-
-const dropdownItems = [
-  "Browse Products",
-  "Edit profile",
-  "View Current Orders",
-  "Checkout"
-]
-
 // @ts-ignore
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -26,7 +14,12 @@ function isDriver(userInfo: PersonalInfo | DriverInfo): userInfo is DriverInfo {
   return (userInfo as DriverInfo).points !== undefined
 }
 
-export default function Banner({ userInfo }: { userInfo: PersonalInfo | DriverInfo }) {
+export default function Banner(
+  { userInfo, showSidebar, setShowSidebar }:
+  { userInfo: PersonalInfo | DriverInfo, showSidebar: boolean, setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>}
+  ) {
+
+  function changeSidebarState() {setShowSidebar(!showSidebar)};
 
   return (
     <Disclosure as="nav" className="bg-light text-dark text-xl font-medium">
@@ -39,51 +32,9 @@ export default function Banner({ userInfo }: { userInfo: PersonalInfo | DriverIn
               <div className="px-1">
                 <div className="flex items-start justify-start">
                   <div className="inset-y-0 left-0 flex">
-
-                    {/* The left-side menu button and its dropdown menu */}
-                    <Menu as="div">
-                      <div>
-                        {/* The button */}
-                        <Menu.Button className="text-xl mb-2 text-dark hover:text-primary">
-                          {dropDownButton.map((item) => (
-                            <a
-                              key={item.name}
-                              className="px-4 pb-2 text-4xl"
-                              aria-current={item.current ? 'page' : undefined}
-                            >
-                              {item.name}
-                            </a>
-                          ))}
-                        </Menu.Button>
-                      </div>
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        {/* The dropdown menu */}
-                        <Menu.Items className="absolute z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          {dropdownItems.map((item) => {
-                            return (
-                              <Menu.Item key={item}>
-                                {({ active }) => (
-                                  <a
-                                    href="#"
-                                    className={classNames(active ? 'bg-lightgray' : '', 'block px-4 py-5')}
-                                  >
-                                    {item}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            );
-                          })}
-                        </Menu.Items>
-                      </Transition>
-                    </Menu>
+                      <button className="text-xl mb-2 text-dark hover:text-primary" onClick={changeSidebarState}>
+                         ≡
+                      </button>
                   </div>
                 </div>
               </div>
--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -2,6 +2,7 @@ import Banner from '../components/Banner/Banner';
 import Sidebar from '../components/Sidebar/Sidebar';
 import Form from '../components/Form/Form';
 import { Outlet } from "@remix-run/react";
+import { useState } from 'react';
 
 export interface PersonalInfo{
   name: String
@@ -24,14 +25,14 @@ const vinDiesel: DriverInfo={
 }
 
 export default function app() {
-
+  let [showSidebar, setShowSidebar] = useState(false);
   return(
     <>
-      <Banner userInfo={ stevenWilde }/>
+      <Banner userInfo={ stevenWilde } showSidebar={showSidebar} setShowSidebar={setShowSidebar}/>
       <div className="flex">
-        <div>
+        {showSidebar && <div>
           <Sidebar />
-        </div>
+        </div>}
         <div className="w-full">
           <Form />
         </div>
