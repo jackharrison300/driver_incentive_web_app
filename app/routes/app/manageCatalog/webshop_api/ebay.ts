10ada433d477c110549798508c67a248ce42b74f
Jacob McFadyen <jsmcfadyen@gmail.com>
2022-11-01 15:20:42 -0400

    deletes e2e

--- app/routes/app/manageCatalog/webshop_api/ebay.ts
+++ app/routes/app/manageCatalog/webshop_api/ebay.ts
@@ -1,6 +1,5 @@
-import axios from "axios";
 export const getEbayItems = async (): Promise<string> => {
-  const res = await axios.get('https://api.kanye.rest');
+  const res = fetch('https://api.kanye.rest');
   await new Promise(r => setTimeout(r, 2000));
-  return res.data.quote;
+  return new Promise(r => "sup");
 };
