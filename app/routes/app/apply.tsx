37c334a8e6ccd9fd80b4a115449489eda0840292
ethanhelwig <ethanahelwig@gmail.com>
2022-11-10 23:05:42 -0500

    driver apply api

--- app/routes/app/apply.tsx
+++ app/routes/app/apply.tsx
@@ -1,5 +1,40 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Role, SsoProvider } from '@prisma/client';
+import { ActionFunction, DataFunctionArgs, json } from '@remix-run/server-runtime';
+import { containsHtml } from '../../shared_functions';
+import { prisma } from '../../../server';
 import * as Yup from 'yup';
+
+export const action: ActionFunction = async ({request}: DataFunctionArgs): Promise<Response> => {
+
+  const form = await request.formData();
+  const [firstName, lastName, email] = [form.get('firstName'), form.get('lastName'), form.get('email')];
+  const errors: {firstName?: string, lastName?: string, email?: string} = {};
+  const name = firstName + " " + lastName;
+  // validate the fields
+  // note: here I'm doing xss validation on input. this is fine for our purposes, but encoding on
+  // output is more robust. see: https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#output-encoding
+  if (typeof firstName !== 'string' || containsHtml(firstName))
+    errors.firstName = 'Invalid first name';
+  if (typeof lastName !== 'string' || containsHtml(lastName))
+    errors.lastName = 'Invalid last name';
+  if (typeof email !== 'string' || containsHtml(email) || !email.includes('@'))
+    errors.email = 'Invalid email address';
+  if (Object.keys(errors).length)
+    return json(errors, { status: 422 });
+
+  // we could add some try catches here for graceful error handling
+  const user = await prisma.user.create({ data: {
+    name: (name as string),
+    email: (email as string), // this typecast is safe because of validation above
+    ssoProvider: SsoProvider.COGNITO_USER_POOL,
+    // TODO: Implement w/ cognito id
+    ssoIdentifier: (email as string),
+    role: Role.ADMIN
+  }});
+  await prisma.admin.create({ data: { userId: user.id }});
+  return new Response(null, {status: 200})
+}
 
 export default function Apply() {
   return (
