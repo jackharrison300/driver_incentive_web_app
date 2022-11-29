c87bc1c90cad7c4705d9c9f7f283643feafe4b6d
Jacob McFadyen <jsmcfadyen@gmail.com>
2022-11-29 14:16:20 -0500

    cleans up repo

--- app/routes/app/manageCatalog/webshop_api/ebay.ts
+++
@@ -1,5 +0,0 @@
-export const getEbayItems = async (): Promise<string> => {
-  const res = fetch('https://api.kanye.rest');
-  await new Promise(r => setTimeout(r, 2000));
-  return new Promise(r => "sup");
-};
